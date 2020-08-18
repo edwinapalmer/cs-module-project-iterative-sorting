5b72da4fdf70450fc31eb317f6a6a80999d62f28
Edwina Palmer <hekamara@gmail.com>
2020-08-18 02:39:18 -0400

    Revert "Complete assignments"
    This reverts commit bef99e828ef867ad3701abdf466a058038ad77ef.
    
    "Complete assignments"

--- Coding Challenge/CC2.py
+++
@@ -1,8 +0,0 @@
-# Print out all of the numbers in the following array that are 
-# divisible by 3:
-
-a = [85, 46, 27, 81, 94, 9, 27, 38, 43, 99, 37, 63, 31, 42, 14]
-
-for i in a:
-    if(i % 3 == 0):
-        print(i)
